7f7a147856e29b8f2c2e045b3cd28b18a19795e3
PLRageman <57705806+PLRageman@users.noreply.github.com>
2019-11-13 10:14:26 +0100

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,6 @@
+echo "# Hejka Bartek" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/PLRageman/Hello-world.git
+git push -u origin master
